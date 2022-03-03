064ba238fbdc0621b3bc7a21a467e3bf6261531f
Joy-Hannn <2362627768@qq.com>
2022-03-03 18:22:24 +0800

    Site updated: 2022-03-03 18:22:22

---
+++ js/crash_cheat.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/joke.ico");
+         document.title = '~🌸可以播放音乐哦🌸~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '~🌸欢迎回来🌸~';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
